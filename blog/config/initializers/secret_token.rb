2f2bcc0e17fcf9aa464a8c72de7898482a7c4688
duckie715 <mikewlongmire@gmail.com>
2012-07-22 22:15:34 -0700

    First Pignite deposit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b602f6818c01638c6e636f15264eed60c37a5ade58c2ef48d00beaabc07f6f2341a02a3b0f0d3cae89452c677d069dc9924ee3a74ac706315992c56901f3b599'
